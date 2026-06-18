72b67cb1fbdc13a66295ee20b7ba50bab955db00
Chethan Kumar gs <chethankumargs770@gmail.com>
2026-06-18 11:03:55 +0530

    Reorganize imports in Toast component

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react'
+import React from 'react'
+import { useEffect } from 'react'
 import { CheckCircle2, AlertCircle, X } from 'lucide-react'
 
 export default function Toast({ message, type = 'success', onClose }) {

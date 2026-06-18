701101339bf2a13f29d6b4ad11aa0ab6d7fb0e0a
Chethan Kumar gs <chethankumargs770@gmail.com>
2026-06-18 11:01:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './context/AuthContext'
 import LoginPage    from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
